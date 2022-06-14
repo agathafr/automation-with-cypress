696ec2d8fc1fd5dde402728a3af3b733288d078f
Agatha França <agathafranca@outlook.com>
2022-06-14 09:06:47 -0300

    Add alura-pic project

---
+++ cypress/e2e/3-alura-pic/alura-pic.cy.js
@@ -0,0 +1,15 @@
+describe('User login and registration', () => {
+    beforeEach(() => {
+        cy.visit('alura-fotos.herokuapp.com');
+    });
+
+    it('Check validation messages', () => {
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Email is required!').should('be.visible');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Email is required!').should('be.visible');
+        cy.contains('ap-vmessage', 'User name is required!').should('be.visible');
+        cy.contains('ap-vmessage', 'Password is required!').should('be.visible');
+    });
+});
