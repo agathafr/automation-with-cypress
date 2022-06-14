8651fddef7bfbb59621c40b271dbfab54c62f440
Agatha França <agathafranca@outlook.com>
2022-06-14 12:26:52 -0300

    Add message appearance validation test cases

--- cypress/e2e/3-alura-pic/alura-pic.cy.js
+++ cypress/e2e/3-alura-pic/alura-pic.cy.js
@@ -12,4 +12,25 @@ describe('User login and registration', () => {
         cy.contains('ap-vmessage', 'User name is required!').should('be.visible');
         cy.contains('ap-vmessage', 'Password is required!').should('be.visible');
     });
+
+    it('Check invalid email message', () => {
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="email"]').type('agatha');
+        cy.contains('ap-vmessage', 'Invalid e-mail').should('be.visible');
+    });
+
+    it('Check password message with less than 8 characters', () => {
+        cy.contains('a', 'Register now').click();
+        cy.get('input[formcontrolname="password"]').type(1);
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Mininum length is 8').should('be.visible');
+    })
+
+    it('Check message must be lower case', () => {
+        cy.contains('a', 'Register now').click();
+        cy.get('input[formcontrolname="userName"]').type('Agatha');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Must be lower case').should('be.visible');
+    })
 });
